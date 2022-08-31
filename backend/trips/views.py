bb0f96ae47f02515b9136a8ee309aee477b05c90
MBisovskyi <mbisovskyi@gmail.com>
2022-08-31 18:23:15 -0400

    Added path to update_trip_by_id for views.py in trips app + created update_trip_by_id method to operate a PUT request

--- backend/trips/views.py
+++ backend/trips/views.py
@@ -3,11 +3,10 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from trips.models import Trip
-
 from trips.serializers import TripSerializer
 
 # Create your views here.
-@api_view(['POST', 'PUT', 'GET'])
+@api_view(['POST', 'GET'])
 @permission_classes([IsAuthenticated])
 def user_trips(request):
     if request.method == 'POST':
@@ -15,3 +14,17 @@ def user_trips(request):
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status = status.HTTP_201_CREATED)
+    if request.method == 'GET':
+        trips = Trip.objects.filter(user_id = request.user.id)
+        serializer = TripSerializer(trips, many=True)
+        return Response(serializer.data)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_trip_by_id(request, pk):
+    if request.method == 'PUT':
+        trip = Trip.objects.get(pk = pk)
+        serializer = TripSerializer(trip, data = request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response(serializer.data)
