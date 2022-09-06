83f334d58650b0d05e72ce0204d9427c8231d1b3
MBisovskyi <mbisovskyi@gmail.com>
2022-09-06 17:18:50 -0400

    react-google-charts installed + Added partial code to filter out data for LineChart to display

--- frontend/src/pages/IncomeDetailsPage/IncomeDetailsPage.js
+++ frontend/src/pages/IncomeDetailsPage/IncomeDetailsPage.js
@@ -6,10 +6,71 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import useAuth from "../../hooks/useAuth";
 
+//Utils
+import React from "react";
+import { Chart } from "react-google-charts";
+import axios from "axios";
+
 const IncomeDetailsPage = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
   const [user, token] = useAuth();
+  const [allTrips, setAllTrips] = useState([]);
+  const [allUserCosts, setAllUserCosts] = useState([]);
+
+  useEffect(() => {
+    const allUserTrips = async () => {
+      let response = await axios.get("http://127.0.0.1:8000/api/trips/", {
+        headers: { Authorization: "Bearer " + token },
+      });
+      setAllTrips(response.data);
+    };
+    const allUserCosts = async () => {
+      let response = await axios.get(
+        `http://127.0.0.1:8000/api/costs/user/${user.id}/`,
+        { headers: { Authorization: "Bearer " + token } }
+      );
+      setAllUserCosts(response.data);
+    };
+    allUserTrips();
+    allUserCosts();
+  }, []);
+
+  let oneYearTime = 86400000 * 365;
+
+  let currentDate = new Date();
+  let currentDateTime = currentDate.getTime();
+
+  let lastYearTrips = allTrips.filter((trip) => {
+    let tripEndedTime = new Date(trip.date_ended);
+    return (
+      tripEndedTime < currentDateTime &&
+      tripEndedTime > currentDateTime - oneYearTime
+    );
+  });
+
+  let lastYearCosts = allUserCosts.filter((cost) => {
+    let costAddedTime = new Date(cost.date_added);
+    return (
+      costAddedTime < currentDateTime &&
+      costAddedTime > currentDateTime - oneYearTime
+    );
+  });
+  console.log(lastYearTrips, lastYearCosts);
+
+  const data = [
+    ["Trip", "Incomes", "Expenses"],
+    ["1", 1000, 400],
+    ["2", 1170, 460],
+    ["3", 660, 1120],
+    ["4", 1030, 540],
+  ];
+
+  const options = {
+    title: "Trips Performance",
+    curveType: "function",
+    legend: { position: "bottom" },
+  };
 
   return (
     <div className="incomedetailspage-wrap">
@@ -24,7 +85,15 @@ const IncomeDetailsPage = () => {
           <p>${state.yearCosts}</p>
         </div>
       </div>
-      <div className="chart-container">Chart</div>
+      <div className="chart-container">
+        <Chart
+          chartType="LineChart"
+          width="100%"
+          height="500px"
+          data={data}
+          options={options}
+        />
+      </div>
     </div>
   );
 };
